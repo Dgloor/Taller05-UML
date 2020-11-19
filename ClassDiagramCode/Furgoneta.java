e09db53667f7bbd8d3c1086ff5033bceed7c2441
joangie <practicatto@gmail.com>
2020-11-18 22:52:32 -0500

    Sin constructor lol

--- ClassDiagramCode/Furgoneta.java
+++ ClassDiagramCode/Furgoneta.java
@@ -4,11 +4,20 @@ public class Furgoneta extends VehiculoPesado{
 	private double pesoSinCarga;
 	private double capacidadCarga;
 
-    public Furgoneta(String numeroRegistro, double capacidadMotor, Date nextFechaPrueba, double pesoCargado, double pesoSinCarga, double capacidadCarga) {
-        super(numeroRegistro, capacidadMotor, nextFechaPrueba, pesoCargado);
+    public double getPesoSinCarga() {
+        return pesoSinCarga;
     }
 
-    
+    public void setPesoSinCarga(double pesoSinCarga) {
+        this.pesoSinCarga = pesoSinCarga;
+    }
 
+    public double getCapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(double capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
 }
 
--- ClassDiagramCode/VehiculoPesado.java
+++ ClassDiagramCode/VehiculoPesado.java
@@ -3,12 +3,18 @@ import java.util.Date;
 public class VehiculoPesado extends Vehiculo{
     private double pesoCargado;
 
-    public VehiculoPesado(String numeroRegistro, double capacidadMotor, Date nextFechaPrueba,double pesoCargado) {
+   public VehiculoPesado(String numeroRegistro, double capacidadMotor, Date nextFechaPrueba,double pesoCargado) {
         super(numeroRegistro, capacidadMotor, nextFechaPrueba);
         this.pesoCargado=pesoCargado;
     }
-    
-    
+
+    public double getPesoCargado() {
+        return pesoCargado;
+    }
+
+    public void setPesoCargado(double pesoCargado) {
+        this.pesoCargado = pesoCargado;
+    } 
 }
 
     
