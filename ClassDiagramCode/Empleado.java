08c1a738ac1e0c9f442a0f0ca38be13998e9b915
Danny Loor <danny_loor00@hotmail.com>
2020-11-18 22:26:52 -0500

    agregación setters & getters

--- ClassDiagramCode/Empleado.java
+++ ClassDiagramCode/Empleado.java
@@ -9,4 +9,28 @@ public class Empleado {
         this.fechaNacimiento = fechaNacimiento;
         this.direccion = direccion;
     }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setFechaNacimiento(Date fechaNacimiento){
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setDireccion(String direccion){
+        this.direccion = direccion;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    public Date getFechaNacimiento(){
+        return this.fechaNacimiento;
+    }
+
+    public String getDireccion(){
+        return this.direccion;
+    }
 }
